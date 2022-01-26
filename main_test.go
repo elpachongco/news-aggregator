db146289481727ad8459505bae278945ce2005b1
elpachongco <earlsiachongco@gmail.com>
2022-01-26 17:19:06 +0800

    Re-format test comments and code

--- main_test.go
+++ main_test.go
@@ -11,46 +11,40 @@ import (
 
 func TestCompare(t *testing.T) {
 
-	// Initialize testing
-	// Make two feeds of same length with different but overlapping items.
-	// prev := [5, 4, 3, 2, 1, 0]
-	// new := [8, 7, 6, 5, 4, 3]
-	maxNum := 8
-	sliceLen := 6
+	// Initialize test variables
 	items := []*gofeed.Item{}
 
-	for k := maxNum; k >= 0; k-- {
+	for k := 19; k >= 0; k-- {
 		var item gofeed.Item
 		item.Title = fmt.Sprintf("Test Item #%d", k)
 		item.Content = fmt.Sprintf("Test Item content #%d", k)
 		items = append(items, &item)
 	}
 
-	var prevFeed gofeed.Feed
-	prevFeed.Title = "Prev Feed"
+	// `items` now has length 20 and contains the values:
+	// ["Test Item content #19"..."Test Item content #0"]
+	// represented in the comments as 
+	// [19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]
 
-	var newFeed gofeed.Feed
+	var prevFeed, newFeed gofeed.Feed
+	prevFeed.Title = "Prev Feed"
 	newFeed.Title = "New Feed"
 
-	// slice `items` now contains the values:
-	// [8,7,6,5,4,3,2,1,0]
-	
-	prevFeed.Items = items[maxNum-sliceLen+1:] // == [5,4,3,2,1,0]
-	newFeed.Items = items[0:sliceLen] // == [8,7,6,5,4,3]
-	// Compare(prev, new) == [8, 7, 6]
+	// Basic test creates two feeds of the same length: prevFeed and newFeed.
+	// prevFeed.Items == [5,4,3,2,1,0],
+	// newFeed.Items == [8,7,6,5,4,3].
+	// Basic test ensures that Compare(prevFeed, newFeed) == [8,7,6].
+	t.Run("Basic test", func(t *testing.T) {
 
-	t.Run("Basic Comparison", func(t *testing.T) {
+		prevFeed.Items = items[14:19] 
+		newFeed.Items = items[11:17] 
 
-		want := items[0: maxNum-sliceLen+1]
-		a := Compare(prevFeed, newFeed) // == [8,7,6]
+		want := items[11:14]
+		a := Compare(prevFeed, newFeed) 
 		for k, v := range a {
 			if v.Title != want[k].Title {
-				t.Errorf("Basic comparison test fail")
+				t.Errorf("Comparison basic test fail")
 			}
 		}
-	})
-
-	t.Run("", func(t *testing.T) {
-
 	})
 }
